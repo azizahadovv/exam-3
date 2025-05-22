ace81da61f23f77fbf4472a9deb51d7422b51ff5
azizahadovv <aziz.ahadovv@yandex.ru>
2025-05-22 16:07:48 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='wiew'>
+      <div className='box'>
+        <h2>Page not found</h2>
+        <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.
+        </p>
+        <hr />
+        <p>If this is your site, and you weren't expecting a 404 for this path, please visit Netlify's <a href="https://www.netlify.com/">"page not found" support guide</a> for troubleshooting tips.</p>
+      </div>
+    </div>
   )
 }
 
